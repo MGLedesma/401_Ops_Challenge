0c5c077c2346a0451258993ac82c3873d7d52a31
MGraceLed <mgraceled@gmail.com>
2022-10-04 23:55:05 -0700

    submit ops challenge 02

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# 401_Ops_Challenge
-A collection of my 401 Cyber Ops challenge in Code Fellows
+# Table of Contents
+
+- [ops01 - Uptime Sensor Tool 1 of 2](class-01.py)
---
+++ class-02.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+# Script Name:  Ops 401 Class 02 Challenge Solution
+# Author:   MaryGrace Ledesma
+# Date of Last Revision: October 04, 2022
+# Description of Purpose: 
+
+# Declaration of variable
+
+
+# Save the output to a text file as a log of events.
+# Accept user input for target IP address.
+pingme = input("Enter IP address to ping: ")
+
+
