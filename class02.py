b36be77457a7dd294e6f2ff491dfa81c0149bcb7
MGraceLed <mgraceled@gmail.com>
2022-10-09 00:48:17 -0700

    update ops challenge 02

--- class02.py
+++ class02.py
@@ -3,17 +3,51 @@
 # Script Name:  Ops 401 Class 02 Challenge Solution
 # Author:   MaryGrace Ledesma
 # Date of Last Revision: October 04, 2022
-# Description of Purpose: 
+# Description of Purpose: Create an uptime sensor tool that sends ICMP packets and evaluate the result.
+
+# Import libraries
+import os, time, datetime
 
 # Declaration of variable
-#now = datetime.datetime.now()
+iphost = input("Enter host to ping: ")
+UP = "Ping Successful"
+DOWN = "Ping Unsuccessful"
 
+# Declaration of Function
+def pingme():
+    print("-" * 50)
+    now = datetime.datetime.now()
+    timestamp = now.strftime('%m-%d-%Y %T')
+
+    response = os.system("ping -c 1 " + iphost)
+    if response == 0:
+        ping_result = UP
+    else:
+        ping_result = DOWN
+
+    print()
+    print(timestamp + f" {iphost}: " + ping_result)
+    print()
+
+# Main
+
+while True:
+    pingme()
+    print("Start: %s" %time.ctime())
+    time.sleep(2)
+    print("End: %s" %time.ctime())
+    
+# End
+
+# Transmit a single ICMP (ping) packet to a specific IP every two seconds.
+# Assign success or failure to a status variable.
+# For every ICMP transmission attempted, print the status variable along with a comprehensive timestamp and destination IP tested.
+# Example output: 2020-10-05 17:57:57.510261 Network Active to 8.8.8.8
+
+# Stretch Goals (Optional Objectives)
 # Save the output to a text file as a log of events.
 # Accept user input for target IP address.
-#pingme = input("Enter IP address to ping: ")
 
-#import datetime
 
-#def check_ping("ping Kali Linux")
-    ping('172.10.0.41', count = 1)
+
 
